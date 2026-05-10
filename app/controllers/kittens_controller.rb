5e8b7c126f53ac206b91ea5dd8a07f70b64e946f
keshiacor <keshiacorioll@me.com>
2026-05-10 19:05:10 -0400

    Adds ability to delete kittens and flash messages

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -15,8 +15,10 @@ class KittensController < ApplicationController
   def create
     @kitten = Kitten.new(kitten_params)
     if @kitten.save
+      flash[:notice] = "We've got a new kitten!"
       redirect_to @kitten
     else
+      flash.now[:alert] = "Hold up, that's not quite right so we didn't save the kitten."
       render :new, status: :unprocessable_entity
     end
   end
@@ -26,15 +28,17 @@ class KittensController < ApplicationController
 
   def update
     if @kitten.update(kitten_params)
+      flash[:notice] = "Kitten updated successfully!"
       redirect_to @kitten
     else
+      flash.now[:alert] = "Hold up, that's not quite right so we didn't update the kitten."
       render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
     if @kitten.destroy
-      redirect_to kittens_path, status: :see_other
+      redirect_to kittens_path, status: :see_other, notice: "Bye bye, kitten! Gone forever."
     else
       redirect_to @kitten, alert: "We couldn't delete the kitten."
     end
